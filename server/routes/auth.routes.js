962d072acb0dd620fa9f04d095be36cb46708202
komangsuryasedana <komangsuryasedana@mail.com>
2024-03-11 18:03:54 +0800

    fix : delete file be

--- server/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { Register, Login, GetUsers } = require("../controller/auth");
-
-router
-  .post("/api/register", Register)
-  .post("/api/login", Login)
-  .get("/api/users/:userId", GetUsers);
-
-module.exports = router;
